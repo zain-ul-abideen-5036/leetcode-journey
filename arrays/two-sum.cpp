ddb5281d2a2197edad5ef38e46d3b453a7d6a7e2
Zain Ul Abideen <142427359+zain-ul-abideen-5036@users.noreply.github.com>
2026-04-12 00:42:55 +0500

    Refactor twoSum to simplify condition check

--- arrays/two-sum.cpp
+++ arrays/two-sum.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i={0}; i<nums.size(); i++){
-            for(int  j={i+1}; j<nums.size(); j++){
-                if(nums[j] == target - nums[i]){
-                    return {i, j};
+        for (int i={0}; i < nums.size(); i++){
+            for (int j={i+1}; j < nums.size(); j++){
+                if (nums[i] + nums[j] == target){
+                    return{i, j};
                 }
+                else{}
             }
         }
-    return {};
+        return {};
     }
 };
